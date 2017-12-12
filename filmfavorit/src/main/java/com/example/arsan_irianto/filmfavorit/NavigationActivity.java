ef7bfa19da8ea17043bd863a589d399dffe21e01
arsan-irianto <arsan.irianto@gmail.com>
2017-12-13 00:59:15 +0800

    Add Tabbar, Blank Fragment and duplicate package from KatalogFilm

--- filmfavorit/src/main/java/com/example/arsan_irianto/filmfavorit/NavigationActivity.java
+++ filmfavorit/src/main/java/com/example/arsan_irianto/filmfavorit/NavigationActivity.java
@@ -3,6 +3,10 @@ package com.example.arsan_irianto.filmfavorit;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +17,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.arsan_irianto.filmfavorit.adapters.SlidingTabAdapter;
+import com.example.arsan_irianto.filmfavorit.fragments.FavouriteFilmFragment;
+import com.example.arsan_irianto.filmfavorit.fragments.SearchFilmFragment;
+
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private SlidingTabAdapter slidingTabAdapter;
+
+    private TabLayout.Tab favouriteFilm;
+    private TabLayout.Tab searchFilm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +38,6 @@ public class NavigationActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +47,57 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initTabBar();
+    }
+
+    private void initTabBar() {
+        //getSupportActionBar().setElevation(0);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        slidingTabAdapter = new SlidingTabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(slidingTabAdapter);
+
+        // Set Tabs item
+        favouriteFilm = tabLayout.newTab();
+        searchFilm = tabLayout.newTab();
+
+        // Labeling tabs item
+        favouriteFilm.setText(R.string.tab_favourite);
+        searchFilm.setText(R.string.tab_search);
+
+        // Set tabs index
+        tabLayout.addTab(favouriteFilm, 0);
+        tabLayout.addTab(searchFilm, 1);
+
+        //set tab selector color
+        tabLayout.setTabTextColors(ContextCompat.getColorStateList(this, R.color.tab_selector));
+
+        //set the indicator
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.colorAccent));
+
+        // switch the fragment when the current fragment was slided.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        // switch the fragment when the tab item was clicked
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     @Override
@@ -77,21 +135,19 @@ public class NavigationActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+            viewPager.setCurrentItem(0);
+            fragment = new FavouriteFilmFragment();
+        } else if (id == R.id.nav_search) {
+            viewPager.setCurrentItem(1);
+            fragment = new SearchFilmFragment();
+        } else if (id == R.id.nav_favourite) {
+            viewPager.setCurrentItem(0);
+            fragment = new FavouriteFilmFragment();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
